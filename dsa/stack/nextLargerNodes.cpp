e4e5743827306957f4e99efcd39fa5664061781c
riyasachdeva04 <117933416+riyasachdeva04@users.noreply.github.com>
2023-06-06 06:40:05 +0000

    stack

---
+++ dsa/stack/nextLargerNodes.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> nextLargerNodes(ListNode* head) {
+        vector<int> ll;
+        while(head)
+        {
+            ll.push_back(head->val);
+            head = head->next;
+        }
+
+        stack<int> st;
+        vector<int> ans(ll.size());
+
+        for(int i=0; i < ll.size(); i++)
+        {
+            while(!st.empty() && ll[i] > ll[st.top()])
+            {
+                ans[st.top()] = ll[i];
+                st.pop();
+            }
+            st.push(i);
+        }
+
+        return ans;
+    }
+};
