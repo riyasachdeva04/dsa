d61c182259c3a8a52b22fc7dae44c3ad5d2faf49
riyasachdeva04 <rsachdeva_be22@thapar.edu>
2023-07-10 02:35:58 +0530

    delete node

--- dsa/binary search tree/deleteNode.cpp
+++ dsa/binary search tree/deleteNode.cpp
@@ -180,55 +180,45 @@ Node* findNodeInBST(Node* root, int target)
 }
 
 
-Node* deleteNodeInBST(Node* root, int target) {
-	
-	//base case
-	if(root == NULL ) {
-		return NULL;
-	}
-//cout << "Request recieved for " >> root->data << " with target" << target << endl;
-	if(root->data == target) {
-		//isi ko delete krna h 
-		//4 cases 
-		if(root->left == NULL && root->right == NULL) {
-			//leaf node
-			//delete root;
-			return NULL;
-		}
-		else if(root->left == NULL && root->right != NULL) {
-			Node* child = root->right;
-			//delete root;
-			return child;
-		}
-		else if(root->left != NULL && root->right == NULL) {
-			Node* child  = root->left;
-			//delete root;
-			return child;
-		}
-		else {
-			//both child
-			//find inorder predecessor inb left subtree
-			int inorderPre = getMax(root->left);
-			//replace root->data value with inorder predecessor
-			root->data = inorderPre;
-			//delete inorder predecessor from left subtree
-			root->left = deleteNodeInBST(root->left,inorderPre);
-			return root;
-			
-		}
-		
-	}
-	else if(target > root -> data) {
-		//right jana chahiye
-		root->right =  deleteNodeInBST(root->right, target);
-	}
-	else if(target < root->data) {
-		//left jana chahioye
-		root->left = deleteNodeInBST(root->left, target);
-	}
-	return root;
-}
+Node* deleteNodeInBST(Node* root, int target)
+{
+  if(root == NULL)
+    return root;
 
+  if(root->data == target)
+  {
+    // 4 cases
+    if(root->left == NULL && root->right == NULL)
+    {
+      return NULL;
+    }
+    else if(root->left == NULL)
+    {
+      Node* child = root->right;
+      return child;
+    }
+    else if(root->right == NULL)
+    {
+      Node* child = root->left;
+      return child;
+    }
+    else
+    {
+      int inOrderPre = getMax(root->left);
+      root->data = inOrderPre;
+      root->left = deleteNodeInBST(root->left, inOrderPre);
+    }
+  }
+  else if(target > root->data)
+  {
+    root->right = deleteNodeInBST(root->right, target);
+  }
+  else
+  {
+    root->left = deleteNodeInBST(root->left, target);
+  }
+  return root;
+}
 
 int main() {
   Node* root = NULL;
