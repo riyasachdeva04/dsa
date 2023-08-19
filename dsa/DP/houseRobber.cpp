ce1eccee687c965a9d2f3b81e59c86f8f487e978
riyasachdeva04 <rsachdeva_be22@thapar.edu>
2023-08-20 02:11:30 +0530

    house robber bottom to top approach

--- dsa/DP/houseRobber.cpp
+++ dsa/DP/houseRobber.cpp
@@ -33,6 +33,28 @@ public:
 
     }
 
+    int solveTab(vector<int>& nums, int n)
+    {
+        vector<int> dp(n+1, 0);
+
+        dp[0] = nums[0];
+
+        for(int i=1; i<=n; i++)
+        {
+            int temp = 0;
+            if(i-2 >= 0)
+                temp = nums[i-2];
+
+            int include = temp + nums[i];
+            int exclude = nums[i-1] + 0;
+            dp[i] = max(include, exclude);
+
+        }
+
+        return dp[n];
+    }
+
+
     int rob(vector<int>& nums) 
     {
         int n = nums.size() - 1;
