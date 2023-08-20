9b2f039052c677d1200ae8e590e86b4734024933
riyasachdeva04 <rsachdeva_be22@thapar.edu>
2023-08-20 21:57:08 +0530

    knapsack dp memo

--- dsa/DP/knapsack.cpp
+++ dsa/DP/knapsack.cpp
@@ -22,7 +22,7 @@ int solveUsingRec(vector<int>& wt, vector<int>& val, int n, int cap)
   return max(incl, excl);
 }
 
-int solveMem(vector<int>& wt, vector<int>& val, int n, int cap, vector<int>& dp)
+int solveMem(vector<int>& wt, vector<int>& val, int n, int cap, vector<vector<int>>& dp)
 {
     if(n == 0)
   {
@@ -32,8 +32,8 @@ int solveMem(vector<int>& wt, vector<int>& val, int n, int cap, vector<int>& dp)
       return 0;
   }
 
-  if(dp[n] != -1)
-    return dp[n];
+  if(dp[n][cap] != -1)
+    return dp[n][cap];
   
   int incl = 0;
   if(wt[n] <= cap)
@@ -41,10 +41,32 @@ int solveMem(vector<int>& wt, vector<int>& val, int n, int cap, vector<int>& dp)
   
   int excl = solveUsingRec(wt, val, n-1, cap);
 
-  dp[n] = max(incl, excl);
-  return dp[n];
+  dp[n][cap] = max(incl, excl);
+  return dp[n][cap];
   
 }
+
+// int solveTab(vector<int>& wt, vector<int>& val, int n, int cap)
+// {
+//   vector<int> dp(n+1, -1);
+
+//   if(wt[0] <= cap)
+//     dp[0] = val[0];
+
+//   for(int i=1; i<=n; i++)
+//     {
+//       int incl = 0;
+//       if(wt[i] <= cap)
+//       {
+//         incl = val[i] + dp[i-1];
+//         cap -= wt[i];
+//       }
+//       int excl = dp[i-1];
+
+//       dp[i] = max(incl, excl);
+//     }
+//   return dp[n];
+// }
 
 int main() {
   vector<int> weights = {4, 5, 1};
@@ -54,7 +76,8 @@ int main() {
   // int ans = solveUsingRec(weights, vals, n-1, cap);
   // cout << ans << endl;
 
-  vector<int> dp(n+1, -1);
+  vector<vector<int> > dp(n+1, vector<int>(cap+1, -1));
   int ans = solveMem(weights, vals, n-1, cap, dp);
   cout << ans << endl;
+
 }
