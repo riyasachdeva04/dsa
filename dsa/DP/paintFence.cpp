ac1cc63afdb1e17de96548e26b06f320829b05eb
riyasachdeva04 <rsachdeva_be22@thapar.edu>
2023-08-20 15:36:01 +0530

    memoization

--- dsa/DP/paintFence.cpp
+++ dsa/DP/paintFence.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include<vector>
 using namespace std;
 
 int paintFence(int n, int k)
@@ -13,11 +14,30 @@ int paintFence(int n, int k)
   return ans;
 }
 
+int solveMem(int n, int k, vector<int>& dp)
+{
+    if(n == 1)
+      return k;
+    if(n == 2)
+      return (k + k*(k-1));
+    
+    if(dp[n] != -1)
+      return dp[n];
+  
+    dp[n] = (solveMem(n - 1, k, dp) + solveMem(n - 2, k, dp))*(k-1);
+  
+    return dp[n];
+}
+
 int main() {
 
   int n = 4;
   int k = 3;
 
-  cout << paintFence(n, k) << endl;
+  // cout << paintFence(n, k) << endl;
+
+  vector<int> dp(n+1, -1);
+  cout << solveMem(n, k, dp) << endl;
+  
 
 }
