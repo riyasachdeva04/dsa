bd2aac7b3f27daa552bf620b855cb61b16bd0349
riyasachdeva04 <rsachdeva_be22@thapar.edu>
2023-08-21 17:34:20 +0530

    knapsack tabulation

--- dsa/DP/knapsack.cpp
+++ dsa/DP/knapsack.cpp
@@ -2,82 +2,90 @@
 #include<vector>
 using namespace std;
 
-// n is last index
-int solveUsingRec(vector<int>& wt, vector<int>& val, int n, int cap)
+//  n= last index
+int solveUsingRec(vector<int>& weights, vector<int>& value, int n, int capacity)
 {
   if(n == 0)
   {
-    if(wt[n] <= cap)
-      return val[n];
+    if(weights[0] <= capacity)
+    {
+      return value[0];
+    }
     else
       return 0;
   }
 
-  int incl = 0;
-  if(wt[n] <= cap)
-    incl = val[n] + solveUsingRec(wt, val, n-1, cap-wt[n]);
-  
-  int excl = solveUsingRec(wt, val, n-1, cap);
-
-  return max(incl, excl);
-}
-
-int solveMem(vector<int>& wt, vector<int>& val, int n, int cap, vector<vector<int>>& dp)
-{
-    if(n == 0)
+  int include = 0;
+  if(weights[n] <= capacity)
   {
-    if(wt[n] <= cap)
-      return val[n];
-    else
-      return 0;
+    include = value[n] + solveUsingRec(weights, value, n-1, capacity-weights[n]);
+    
   }
+  int exclude = solveUsingRec(weights, value, n-1, capacity);
 
-  if(dp[n][cap] != -1)
-    return dp[n][cap];
-  
-  int incl = 0;
-  if(wt[n] <= cap)
-    incl = val[n] + solveUsingRec(wt, val, n-1, cap-wt[n]);
-  
-  int excl = solveUsingRec(wt, val, n-1, cap);
-
-  dp[n][cap] = max(incl, excl);
-  return dp[n][cap];
-  
+  int ans = max(include, exclude);
+  return ans;
 }
 
-// int solveTab(vector<int>& wt, vector<int>& val, int n, int cap)
-// {
-//   vector<int> dp(n+1, -1);
+// n is last index
+int solveMem(vector<int>& weights, vector<int>& value, int n, int capacity, vector<vector<int>>& dp)
+{
+  if(n == 0)
+  {
+    if(weights[n] <= capacity)
+      return value[n];
+  }
 
-//   if(wt[0] <= cap)
-//     dp[0] = val[0];
+  if(dp[n][capacity] != -1)
+    return dp[n][capacity];
+  
+  int include = 0;
+  if(weights[n] <= capacity)
+  {
+    include = value[n] + solveUsingRec(weights, value, n-1, capacity-weights[n]);
+    
+  }
+  int exclude = solveUsingRec(weights, value, n-1, capacity);
 
-//   for(int i=1; i<=n; i++)
-//     {
-//       int incl = 0;
-//       if(wt[i] <= cap)
-//       {
-//         incl = val[i] + dp[i-1];
-//         cap -= wt[i];
-//       }
-//       int excl = dp[i-1];
+  dp[n][capacity] = max(include, exclude);
+  return dp[n][capacity];
+}
 
-//       dp[i] = max(incl, excl);
-//     }
-//   return dp[n];
-// }
+// n is the last index
+int solveUsingTab(vector<int>& weights, vector<int>& value, int n, int capacity)
+{
+  vector<vector<int>> dp(n+1, vector<int>(capacity+1, 0));
 
+  for(int w=weights[0]; w<=capacity; w++)
+    {
+      if(weights[0] <= capacity)
+        dp[0][w] = value[0];
+    }
+
+  for(int index=1; index<=n; index++)
+    {
+      for(int wt = 0; wt<=capacity; wt++)
+        {
+          int include = 0;
+          if(weights[index] <= wt)
+          {
+            include = value[index] + dp[index-1][wt-weights[index]];
+            
+          }
+          int exclude = dp[index-1][wt];
+
+          dp[index][wt] = max(include, exclude);
+        }
+    }
+  return dp[n][capacity];
+}
 int main() {
   vector<int> weights = {4, 5, 1};
-  vector<int> vals = {1, 2, 3};
+  vector<int> value = {1, 2, 3};
   int n = 3;
   int cap = 4;
-  // int ans = solveUsingRec(weights, vals, n-1, cap);
-  // cout << ans << endl;
 
-  vector<vector<int> > dp(n+1, vector<int>(cap+1, -1));
-  int ans = solveMem(weights, vals, n-1, cap, dp);
-  cout << ans << endl;
-
+  // vector<vector<int>> dp(n, vector<int>(cap+1, -1));
+  
+  cout << solveUsingTab(weights, value, n-1, cap);
 }
