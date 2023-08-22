31cc772079073cf5dca59ec5a9dc0ba87e003e2d
riyasachdeva04 <rsachdeva_be22@thapar.edu>
2023-08-23 02:49:10 +0530

    space optim

--- dsa/DP/diceRollsToTarget.cpp
+++ dsa/DP/diceRollsToTarget.cpp
@@ -76,11 +76,42 @@ public:
         return dp[n][target];
     }
 
+    int SOsoln(int n, int k, int target)
+    {
+        long long int MOD = 1000000007;
+
+        vector<int> prev(target+1, 0);
+        vector<int> curr(target+1, 0);
+
+        prev[0] = 1;
+
+        for(int index=1; index<=n; index++)
+        {
+            for(int t=1; t<=target; t++)
+            {
+                long long int ans = 0;
+                for(int i=1; i<=k; i++)
+                {
+                    long long int recAns = 0;
+                    if(t-i >= 0)
+                        recAns = prev[t-i];
+                    ans = (ans%MOD)+(recAns%MOD);
+                    ans = ans%MOD;
+                }
+                curr[t] = ans;
+
+            }
+            prev = curr;
+        }
+
+        return curr[target];
+    }
+
     int numRollsToTarget(int n, int k, int target) {
 
         // return solveUsingRec(n, k, target);
         // vector<vector<long long int>> dp(n+1, vector<long long int>(target+1, -1));
-        return solveUsingTab(n, k, target);
+        return SOsoln(n, k, target);
 
     }
 
